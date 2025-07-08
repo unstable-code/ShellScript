24e46c3a3ce18722ff36084837782c2cd4c7ec80
HyeongminKim <assa0620@gmail.com>
2025-07-08 20:18:15 +0900

    잘못된 객체를 추적하던 문제 수정

--- Universal/Userscripts/라프텔 줄거리 확장.js
+++ Universal/Userscripts/라프텔 줄거리 확장.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://laftel.net/*
 // @grant       none
-// @version     2025.02010
+// @version     2025.07080
 // @author      Hyeongmin Kim
 // @description 2/1/2025, 10:55:01 PM
 // @updateURL   https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/%EB%9D%BC%ED%94%84%ED%85%94%20%EC%A4%84%EA%B1%B0%EB%A6%AC%20%ED%99%95%EC%9E%A5.js
@@ -11,11 +11,13 @@
 // ==/UserScript==
 
 const observer = new MutationObserver(() => {
-  const elements = document.querySelectorAll('.fmEjHp');
-
-  elements.forEach(element => {
-    element.style.webkitLineClamp = '5';
-  });
+  const el = document.querySelector('.fLCArm');
+  if (el) {
+    el.style.setProperty('-webkit-line-clamp', 'unset', 'important');
+    el.style.setProperty('display', 'block', 'important');
+    el.style.setProperty('-webkit-box-orient', 'unset', 'important');
+    el.style.setProperty('overflow', 'visible', 'important');
+  }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
