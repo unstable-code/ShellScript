fd51f4df8398025b6b8ac8b2b2ec58f1010ae361
unstable-code <assa0620@gmail.com>
2025-09-16 16:17:13 +0900

    버튼 활성화가 되지 않던 문제 수정

--- Universal/Userscripts/AWS S3 영구삭제 자동 동의.js
+++ Universal/Userscripts/AWS S3 영구삭제 자동 동의.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS S3 영구 삭제 자동 입력
 // @namespace    http://tampermonkey.net/
-// @version      2025.09160
+// @version      2025.09161
 // @description  placeholder가 "영구 삭제"이면 value에 자동 입력
 // @match        https://*.console.aws.amazon.com/s3/buckets/*
 // @grant        none
@@ -12,27 +12,30 @@
 (function() {
     'use strict';
 
-    function setValueForPermanentDelete(input) {
+    // value 설정 + 이벤트 발생 함수
+    function setValueAndTrigger(input) {
         if (input.placeholder === "영구 삭제" && input.value !== "영구 삭제") {
             input.value = "영구 삭제";
+
+            // React/SPA에서 인식시키기 위해 이벤트 강제 발생
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+            input.dispatchEvent(new Event('change', { bubbles: true }));
         }
     }
 
-    // 기존에 이미 있는 input 요소 처리
-    document.querySelectorAll('input').forEach(setValueForPermanentDelete);
+    // 페이지 로드 시 이미 존재하는 input 처리
+    document.querySelectorAll('input').forEach(setValueAndTrigger);
 
-    // 동적으로 생성되는 input을 감지
+    // 동적으로 생성되는 input 감지
     const observer = new MutationObserver(mutations => {
         for (const mutation of mutations) {
             if (mutation.type === 'childList') {
                 mutation.addedNodes.forEach(node => {
                     if (node.nodeType === Node.ELEMENT_NODE) {
-                        // 새로 추가된 input 요소가 있다면 처리
-                        if (node.tagName === 'INPUT') {
-                            setValueForPermanentDelete(node);
-                        }
-                        // 하위에 input이 있다면 처리
-                        node.querySelectorAll?.('input').forEach(setValueForPermanentDelete);
+                        // 새로 추가된 input 처리
+                        if (node.tagName === 'INPUT') setValueAndTrigger(node);
+                        // 하위에 있는 input 처리
+                        node.querySelectorAll?.('input').forEach(setValueAndTrigger);
                     }
                 });
             }
@@ -41,4 +44,3 @@
 
     observer.observe(document.body, { childList: true, subtree: true });
 })();
-
