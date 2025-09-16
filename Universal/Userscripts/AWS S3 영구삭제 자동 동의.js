fe18349ccb08bbe4821db003951fcbde724a8489
unstable-code <assa0620@gmail.com>
2025-09-16 16:08:43 +0900

    업데이트 URL 추가

--- Universal/Userscripts/AWS S3 영구삭제 자동 동의.js
+++ Universal/Userscripts/AWS S3 영구삭제 자동 동의.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         AWS S3 영구 삭제 자동 입력
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      2025.09160
 // @description  placeholder가 "영구 삭제"이면 value에 자동 입력
 // @match        https://*.console.aws.amazon.com/s3/buckets/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/AWS%20S3%20%EC%98%81%EA%B5%AC%EC%82%AD%EC%A0%9C%20%EC%9E%90%EB%8F%99%20%EB%8F%99%EC%9D%98.js
+// @downloadURL  https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/AWS%20S3%20%EC%98%81%EA%B5%AC%EC%82%AD%EC%A0%9C%20%EC%9E%90%EB%8F%99%20%EB%8F%99%EC%9D%98.js
 // ==/UserScript==
 
 (function() {
