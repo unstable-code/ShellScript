c0f6b51ac8fbde867faa5a379d75ec332bfe462f
unstable-code <assa0620@gmail.com>
2025-09-29 19:53:54 +0900

    프로퍼티 이름 수정

--- Universal/Userscripts/라프텔 줄거리 확장.js
+++ Universal/Userscripts/라프텔 줄거리 확장.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://laftel.net/*
 // @grant       none
-// @version     2025.07100
+// @version     2025.09290
 // @author      Hyeongmin Kim
 // @description 2/1/2025, 10:55:01 PM
 // @updateURL   https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/%EB%9D%BC%ED%94%84%ED%85%94%20%EC%A4%84%EA%B1%B0%EB%A6%AC%20%ED%99%95%EC%9E%A5.js
@@ -11,15 +11,17 @@
 // ==/UserScript==
 
 const observer = new MutationObserver(() => {
-  const el = document.querySelector('.fLCArm');
-  if (el) {
-    el.style.setProperty('-webkit-line-clamp', 'unset', 'important');
-    el.style.setProperty('display', 'block', 'important');
-    el.style.setProperty('-webkit-box-orient', 'unset', 'important');
-    el.style.setProperty('overflow', 'visible', 'important');
-  }
+  document.querySelectorAll('*').forEach(el => {
+    const style = getComputedStyle(el);
+    if (style.webkitLineClamp !== 'none') {   // 기본값은 'none'
+      el.style.setProperty('-webkit-line-clamp', 'unset', 'important');
+      el.style.setProperty('display', 'block', 'important');
+      el.style.setProperty('overflow', 'visible', 'important');
+      el.style.setProperty('-webkit-box-orient', 'unset', 'important');
+    }
+  });
 
-  document.querySelectorAll('span.sc-b12ebb9a-4.lgYMql').forEach(span => {
+  document.querySelectorAll('.dMMyar').forEach(span => {
     if (span.textContent.includes('방영중') && !span.dataset.colored) {
       span.style.fontWeight = 'bold';
       span.style.color = 'red';
