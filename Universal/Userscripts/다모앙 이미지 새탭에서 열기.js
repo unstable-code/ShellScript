70e29401c5fc4af58fb5159ca43ee30e89f855e9
unstable-code <assa0620@gmail.com>
2026-02-22 17:25:41 +0900

    업데이트 주소 추가

--- Universal/Userscripts/다모앙 이미지 새탭에서 열기.js
+++ Universal/Userscripts/다모앙 이미지 새탭에서 열기.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         다모앙 게시글 이미지 뷰어 활성화
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2026.02220
 // @description  특정 클래스 내의 이미지를 클릭하면 새 탭에서 원본 이미지를 엽니다.
 // @author       unstable-code
 // @match        *://damoang.net/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EC%9D%B4%EB%AF%B8%EC%A7%80%20%EC%83%88%ED%83%AD%EC%97%90%EC%84%9C%20%EC%97%B4%EA%B8%B0.js
+// @downloadURL  https://raw.githubusercontent.com/unstable-code/ShellScript/refs/heads/master/Universal/Userscripts/%EB%8B%A4%EB%AA%A8%EC%95%99%20%EC%9D%B4%EB%AF%B8%EC%A7%80%20%EC%83%88%ED%83%AD%EC%97%90%EC%84%9C%20%EC%97%B4%EA%B8%B0.js
 // ==/UserScript==
 
 (function() {
