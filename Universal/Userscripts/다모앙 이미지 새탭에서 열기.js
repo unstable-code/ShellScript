03aaf67df04bfa95bf64ac105966e00cf76b57b2
unstable-code <assa0620@gmail.com>
2026-02-22 18:27:08 +0900

    코멘트에 첨부된 이미지도 지원하도록 개선

--- Universal/Userscripts/다모앙 이미지 새탭에서 열기.js
+++ Universal/Userscripts/다모앙 이미지 새탭에서 열기.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         다모앙 게시글 이미지 뷰어 활성화
 // @namespace    http://tampermonkey.net/
-// @version      2026.02220
+// @version      2026.02221
 // @description  특정 클래스 내의 이미지를 클릭하면 새 탭에서 원본 이미지를 엽니다.
 // @author       unstable-code
 // @match        *://damoang.net/*
@@ -13,8 +13,10 @@
 (function() {
     'use strict';
 
+    const targetSelector = '.se-component.se-image-container img, img.img-fluid';
+
     document.addEventListener('click', function(e) {
-        const img = e.target.closest('.se-component.se-image-container img');
+        const img = e.target.closest(targetSelector);
 
         if (img && img.src) {
             e.preventDefault();
@@ -25,6 +27,6 @@
     }, true);
 
     const style = document.createElement('style');
-    style.innerHTML = '.se-component.se-image-container img { cursor: pointer !important; }';
+    style.innerHTML = `${targetSelector} { cursor: pointer !important; }`;
     document.head.appendChild(style);
 })();
